4a85a49ea21cd593d657b5226b175c47a91a2d56
shubham44-code <shubham162001@gmail.com>
2020-10-14 11:25:17 +0530

    Day1

---
+++ Day 1/Day1(2).js
@@ -0,0 +1,60 @@
+// 😎
+// JavaScript Literals
+// 1. Numbers are written with or without decimals:
+// 5
+// 8.2
+
+// 😎
+// 2. Strings are text, written within double or single quotes:
+// "Coder Boy"
+
+// 'Coder Boy'
+
+
+// 😎
+// JavaScript Variables:-variables are used to store data values
+
+var a;
+a = 10;
+console.log(a);//output:- 10
+
+
+var b;
+b = "hello";
+console.log(b); //output: - hello
+
+
+// how to check type of variable in javascript❓
+// The typeof operator returns a string indicating the type of the unevaluated operand
+
+console.log(typeof(a));//output: - number
+console.log(typeof(b));//output: - string
+
+
+
+
+// Null & Undefined;
+var k;
+var l = null;
+console.log(k);
+console.log(l);
+
+// how to print "hello world"(with double quotes) with colon
+
+// \ before "
+
+// ex:-
+// console.log(""Hello World"");❌
+console.log("\"Hello World\"");
+
+
+
+// comments 👓
+// Single line comments start with //
+// Multi-line comments start with /* and end with */
+
+
+// How To Use Emojis On Windows
+// win + .
+// or
+// win + ;
---
+++ Day 1/Day1.js
@@ -0,0 +1,8 @@
+// The write() method writes HTML expressions or JavaScript code to a document
+document.write("HEllo World");
+
+//  alert box to display data
+window.alert("Hello World");
+
+// For debugging purposes, you can call the console.log() method in the browser to display data
+console.log("Hello World");
