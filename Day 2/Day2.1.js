6201692a65d4a3b3948f4b77244db3b7c31f7892
shubham44-code <shubham162001@gmail.com>
2020-10-15 15:57:24 +0530

    Day 2

---
+++ Day 2/Day2.1.js
@@ -0,0 +1,49 @@
+// JavaScript Operators⏩⏩
+// (=)assignment operator assigns a value to a variable.
+
+var a = 8;
+
+// (+)  addition : (-) Subtraction 
+var k = 8;
+var l = 6;
+var kl = k + l;
+console.log(kl);
+// Output:- 14
+var kl = k - l;
+console.log(kl);
+// Output:- 2
+
+
+
+// (*) multiplication : (/) Division
+var a = 4;
+var b = 2;
+var ab = a * b;
+console.log(ab);
+// Output:- 8
+var ab = a / b;
+console.log(ab);
+// Output:- 2
+
+
+// (**) Exponentiation  : (%) Modulus 
+var d = 4;
+var e = 2;
+var de = d ** e;
+console.log(de);
+// Output:- 16
+var de = d % e;
+console.log(de);
+// Output:- 0
+
+
+
+// (++) Increment  : (--) Decrement 
+var k = 4;
+k++;
+console.log(k);
+// Output:- 5
+var l = 4;
+l--;
+console.log(l);
+// Output:- 3
