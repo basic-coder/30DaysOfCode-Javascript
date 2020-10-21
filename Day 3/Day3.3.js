5c51687bf6f07b09f0b4087a799ea2c0f41c28bd
shubham44-code <shubham162001@gmail.com>
2020-10-21 14:57:50 +0530

    Day 3

---
+++ Day 3/Day3.3.js
@@ -0,0 +1,18 @@
+// Events in JS
+// Events are actions or occurrences that happen in the system you are programming, which the system tells you about so you can 
+// respond to them in some way if desired. 
+
+// syntax
+/* <element event="some JavaScript"> */
+
+// ex:-
+    // html👇👇
+    /* <button onclick="show()">click</button> */
+
+    // JS👇
+    function show(){
+        console.log("Button Clicked");
+    }
+
+// HTML Events
+// onchange,onclick,onmouseover,onmouseout,onkeydown,onload
