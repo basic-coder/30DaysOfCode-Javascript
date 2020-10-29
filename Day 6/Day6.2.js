89431f8abd54959e361d379fe747231d88543a17
shubham44-code <shubham162001@gmail.com>
2020-10-29 14:55:36 +0530

    Day 6

---
+++ Day 6/Day6.2.js
@@ -0,0 +1,57 @@
+// Conditional Statements😍😍
+// if statement😍😍
+// syntax:- 
+// if(condition){
+//        code
+// }
+
+// if condition is true :- block inside if statement is executed
+// ex:- 
+var a = 5;
+if(a > 1){
+    console.log(a + " is greater than 1");
+}
+//output:- 5 is greater than 1
+
+
+// if else statement😍😍
+// syntax:- 
+// if(condition){
+//        code
+// }else{
+//       code
+// }
+
+
+// if condition is true :- block inside if statement is executed
+//if condition is false :- block inside else statement is executed
+// ex:- 
+var a = 0;
+if(a > 1){
+    console.log(a + " is greater than 1");
+}else{
+    console.log(a + " is less than 1");
+}
+//output:- 0 is less than 1
+
+
+// else  if statement😍😍
+// it is used to check multiple condition
+// syntax:- 
+// if(condition){
+//        code
+// }else if(condition{
+//       code
+// }else{
+//       code 
+// }
+
+var a = 1;
+if(a > 1){
+    console.log(a + " is greater than 1");
+}else if(a==1){
+    console.log("equal values");
+}else{
+    console.log(a + " is less than 1");
+}
+//output:- equal values
